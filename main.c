7ccfca29ac0270b3486284ed3237d6663d5eee3c
Mohamed Gamal (Jimmy) <jimadness12@gmail.com>
2015-04-03 14:47:18 +0200

    Main function & Check function

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void check(char*);
+
+int main()
+{
+   char cmd[1000];
+   char *token;
+   printf("\t\t**Welcome to the 'M&J' interpreter V1.0**\n\n");
+   while(1){
+    printf("M&J>>> ");
+    gets(cmd);
+    token=strtok(cmd," ");
+    while(token){
+        check(token);
+        token=strtok(NULL," ");
+    }
+   }
+    return 0;
+}
+void check(char* input){
+if(!strcmp(input,"exit"))
+    exit(0);
+//else if(strcmp(input,""))
+}
